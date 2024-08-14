0c90fb20586ec6caaaac91c883c902042265706e
dtkdtk <dtkdtk0.mail@gmail.com>
2024-08-15 00:15:58 +0300

    Use Prettier

--- src/core/ApiVersion.ts
+++ src/core/ApiVersion.ts
@@ -1,29 +1,29 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+import { readFileSync } from "fs"
+import { join } from "path"
 import { BB_Requests } from "api-types"
 import { App, RequestHandler, Routable } from "utility-types"
 
-export class ApiVersion implements Routable{
-	private packageJson: any;
+export class ApiVersion implements Routable {
+  private packageJson: any
 
-	initializeRoutes(app: App): void {
-		app.route({
-			url: "/apiVersion",
-			method: "GET",
-			
-			handler: this.#requestHandler.bind(this),
-		});
-  }          
+  initializeRoutes(app: App): void {
+    app.route({
+      url: "/apiVersion",
+      method: "GET",
 
-	#requestHandler: RequestHandler<BB_Requests["/apiVersion"]> = async (
-		req,
-		res
-	) => {
-		const packageJsonPath = join(__dirname, '../../package.json');
-		this.packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-		return res.send({
-			apiVersion: this.packageJson.apiVersion,
-			gameVersion: this.packageJson.gameVersion
-		}); 
-	}
+      handler: this.#requestHandler.bind(this),
+    })
+  }
+
+  #requestHandler: RequestHandler<BB_Requests["/apiVersion"]> = async (
+    req,
+    res
+  ) => {
+    const packageJsonPath = join(__dirname, "../../package.json")
+    this.packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"))
+    return res.send({
+      apiVersion: this.packageJson.apiVersion,
+      gameVersion: this.packageJson.gameVersion,
+    })
+  }
 }
