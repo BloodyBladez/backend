91db3b8f48980c7af5ef2f77826a7abea60af585
dtkdtk <dtkdtk0.mail@gmail.com>
2024-08-19 23:19:20 +0300

    Fix user storage

--- src/core/User.ts
+++ src/core/User.ts
@@ -1,7 +1,8 @@
+import { ArrayStorage } from "@eds-fw/storage"
+import { UserData } from "bloodybladez-api-types"
+import { randomBytes } from "crypto"
 import path from "path"
 import { AuthSecret } from "./AuthSecret.js"
-import { randomBytes } from "crypto"
-import { ArrayStorage } from "@eds-fw/storage"
 
 /**
  * Базовый пользователь. Никак не связан с игрой; существует абстрактно.
@@ -10,7 +11,7 @@ export class User {
   static readonly USER_ID_LENGTH = 16
 
   /** Является ссылкой на данные в `User.storage` */
-  readonly data: Readonly<User.Data>
+  readonly data: Readonly<UserData>
 
   lobbyId: string | undefined
 
@@ -43,7 +44,7 @@ export class User {
   /**
    * @param creationData Сериализуемые данные пользователя. Записываются в `User.storage`
    */
-  static create(creationData: Pick<User.Data, "login" | "password">): User {
+  static create(creationData: Pick<UserData, "login" | "password">): User {
     const maybeExistsUser = this.instances.find(
       (it) => it.data.login == creationData.login
     )
@@ -58,6 +59,10 @@ export class User {
     }
     const user = new User(fullData)
     User.instances.push(user)
+    //если мы создаём пользователей из хранилища,
+    //то кол-во инстанций не будет равно длине хранилищ
+    //(...пока мы не проитерируем всех пользователей)
+    if (this.instances.length == this.storage.length) this.storage.save()
     return user
   }
 
@@ -73,7 +78,7 @@ export class User {
   /**
    * Сырое хранилище **долгосрочных** данных о пользователе
    */
-  static readonly storage: ArrayStorage<User.Data>
+  static readonly storage: ArrayStorage<UserData>
 
   static loadFromStorage(): void {
     ;(this.storage as any) = ArrayStorage.create(
@@ -90,18 +95,9 @@ export class User {
     for (const userData of storageCopy) User.instances.push(new User(userData))
   }
 
-  private constructor(data: User.Data) {
+  private constructor(data: UserData) {
     const index = User.storage.push(data) - 1
-    User.storage.save()
     this.data = User.storage[index]
     AuthSecret.createAccountKey(data.id)
-  }
-}
-export namespace User {
-  export interface Data {
-    id: string
-    /** username */
-    login: string
-    password: string | null
   }
 }
